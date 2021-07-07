8a70b9c85531b5951bfbb44aa20aaad1d899fe58
karanlakhwani0910 <karan.lakhwani23@gmail.com>
2021-07-07 17:00:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Table, Container, Button } from "react-bootstrap";
+
+import { useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = () => {
+    setUsers([])
+    for (let i = 0; i < 10; i++) {
+      fetch("https://randomuser.me/api/")
+        .then((res) => res.json())
+        .then((json) => {
+          setUsers((users) => [...users, json]);
+        });
+    }
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="container">
+      <div className="text-center">
+        <Button variant="dark" onClick={getUsers}>
+          Generate Users
+        </Button>
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+          {
+            users.map((user)=>{
+              return(
+                <tr key={user.info.seed}>
+                  <td>{user.results[0].name.first}</td>
+                  <td>{user.results[0].name.last}</td>
+                  <td>{user.results[0].email}</td>    
+                  <td>{user.results[0].phone}</td>              
+                </tr>
+              )
+            })
+          }
+          
+        </tbody>
+      </Table>
+    </Container>
   );
 }
 
